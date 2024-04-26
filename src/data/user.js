a5f9e524e69191f8d862c838e36a42b757efd9a3
flakegotgame <victorchange808@gmail.com>
2024-04-26 21:00:16 +0300

    initial commit

--- src/data/user.js
+++ src/data/user.js
@@ -1,6 +1,9 @@
-const username = "Liza";
+const usernmae = "Liza";
 const city = "New York";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
+export const username = "Liza";
+export const kv = "New York";
+export const imag= "https://i.imgur.com/mV8PQxj.gif";
 
-// export all three variables as named exports
+// export all three variables as named exportsjg
